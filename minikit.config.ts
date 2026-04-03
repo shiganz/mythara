9c14d7807f184b2495a62360cb40eeb27118af4c
Dens <153103221+shiganz@users.noreply.github.com>
2026-04-03 13:03:47 +0800

    feat: update minikit.config.ts with account association

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,31 +1,30 @@
-const ROOT_URL =
-  process.env.NODE_ENV === "production"
-    ? `${process.env.NEXT_PUBLIC_ROOT_URL}`
-    : "http://localhost:3000";
+const ROOT_URL = "https://mythara-zeta.vercel.app";
 
 export const minikitConfig = {
   accountAssociation: {
-    header: "",
-    payload: "",
-    signature: "",
+    header: "eyJmaWQiOjIzNzk2NzMsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhCM0M5OEU5RWY0M2Y3ODJEZWIwOTVjMGM1NTcyRjZCNjQ4QzZENDExIn0",
+    payload: "eyJkb21haW4iOiJteXRoYXJhLXpldGEudmVyY2VsLmFwcCJ9",
+    signature: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEEyOFZGB86NWiCzCdVBidoWcqslVo7FW12CYqapteFt_ld4xlJinP9oh6_o7HNgXKjvGnz_lFfjXSgjzOJ44hMKGwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
   },
   miniapp: {
     version: "1",
-    name: "Miniapp Name",
-    subtitle: "Miniapp subtitle",
-    description: "Miniapp Description",
-    screenshotUrls: [`${ROOT_URL}/image.png`],
-    iconUrl: `${ROOT_URL}/image.png`,
-    splashImageUrl: `${ROOT_URL}/image.png`,
-    splashBackgroundColor: "#000000",
+    name: "Mythara",
+    subtitle: "Mystery Game",
+    description: "A mystery box game",
+    screenshotUrls: ["https://mythara-zeta.vercel.app/image.png"],
+    iconUrl: "https://mythara-zeta.vercel.app/image.png",
+    splashImageUrl: "https://mythara-zeta.vercel.app/image.png",
+    splashBackgroundColor: "#7739ea",
     homeUrl: ROOT_URL,
-    webhookUrl: `${ROOT_URL}/api/webhook`,
-    primaryCategory: "social",
-    tags: ["marketing", "ads", "quickstart"],
-    heroImageUrl: `${ROOT_URL}/image.png`,
-    tagline: "your tagline here",
-    ogTitle: "Miniapp Name",
-    ogDescription: "Miniapp Description",
-    ogImageUrl: `${ROOT_URL}/image.png`,
+    webhookUrl: "https://mythara-zeta.vercel.app/api/webhook",
+    primaryCategory: "games",
+    tags: ["mystery", "miniapp", "adventure"],
+    heroImageUrl: "https://mythara-zeta.vercel.app/image.png",
+    tagline: "Open Mystery Boxes!",
+    ogTitle: "Mythara",
+    ogDescription: "A mystery box game",
+    ogImageUrl: "https://mythara-zeta.vercel.app/image.png",
+    "noindex": true
+
   },
 } as const;
